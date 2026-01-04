8179b82f331a7f7da5fc519478d043bf70dfed32
Moh Sopyan <mohsopyan29@gmail.com>
2026-01-04 19:52:24 +0900

    add callback-based dynamic filter function

--- 07-dynamic-function.js
+++ 07-dynamic-function.js
@@ -12,3 +12,22 @@ let data = [1, 2, 3, 5, 6]
 
 console.log(filterKelipatan(data, 2))
 console.log(filterKelipatan(data, 5))
+
+function filterDenganAturan(arr, aturan) {
+    let hasil = []
+
+    for (let i = 0; i < arr.length; i++) {
+        if(aturan(arr[i])) {
+            hasil.push(arr[i])
+        }
+    }
+
+    return hasil;
+}
+
+function isGenap(angka) {
+    return angka % 2 === 0
+}
+
+let data2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+console.log(filterDenganAturan(data2, isGenap))
